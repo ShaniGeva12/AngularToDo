13e31aad3d33c722b48186962b840228535a89d4
Shani <shanigeva12@gmail.com>
2021-05-12 23:29:57 +0300

    updating packages & angular

--- src/app/components/to-do-item/to-do-item.component.spec.ts
+++ src/app/components/to-do-item/to-do-item.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ToDoItemComponent } from './to-do-item.component';
 
@@ -6,7 +6,7 @@ describe('ToDoItemComponent', () => {
   let component: ToDoItemComponent;
   let fixture: ComponentFixture<ToDoItemComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ToDoItemComponent ]
     })
